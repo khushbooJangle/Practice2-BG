7e34aa3f3ba4e9ea9b4277b778443d89107b2dd2
khushbooJangle <khushboojangle@gmail.com>
2019-10-14 20:21:12 +0530

    modified

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
+var c1 = document.querySelector(".color1");
+var c2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
 function setGradient() {
 	body.style.background = 
 	"linear-gradient(to right, " 
-	+ color1.value 
+	+ c1.value 
 	+ ", " 
-	+ color2.value 
+	+ c2.value 
 	+ ")";
 
 	css.textContent = body.style.background + ";";
 }
 
-color1.addEventListener("input", setGradient);
+c1.addEventListener("input", setGradient);
 
-color2.addEventListener("input", setGradient);
+c2.addEventListener("input", setGradient);
